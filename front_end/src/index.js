3f10166b5177c0b1ebc5f7c360459e4e6a507fb3
wenshuyu <wenhsuyu991@hellobike.com>
2023-12-25 16:25:24 +0800

    怎么整

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { createRoot } from "react-dom/client"; // 引入createRoot
+import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./contexts/AuthContext";
 import App from "./App";
 
 const container = document.getElementById("root");
-const root = createRoot(container); // 使用createRoot创建根
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
