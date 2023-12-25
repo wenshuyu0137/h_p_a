ae9d337978e5a40df32ef0381ada8608910ce1e8
wenshuyu <wenhsuyu991@hellobike.com>
2023-12-25 14:02:33 +0800

    测试

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -16,7 +16,7 @@ function App() {
   const { isAuthenticated } = useAuth();
 
   return (
-    <Router basename="/agent">
+    <Router basename="/">
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
